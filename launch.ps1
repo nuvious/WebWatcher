8f648a85883bef140a07200b4ba75af2a9f3a47a
David Cheeseman <nuvious@gmail.com>
2021-12-28 12:04:05 -0500

    Added mkdir commands to launch scripts

--- launch.ps1
+++ launch.ps1
@@ -1,2 +1,3 @@
+New-Item -ItemType Directory -Force -Path ${PWD}\data
 docker build -t webwatcher .
 docker run -d -v ${PWD}/data:/data --env-file ./env.list webwatcher
--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir -p data
 docker build -t webwatcher .
 docker run -d $PWD/data:/data --env-file env.list webwatcher
