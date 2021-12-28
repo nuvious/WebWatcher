34878960402871078524329785db55ad0359e895
David Cheeseman <nuvious@gmail.com>
2021-12-28 18:45:30 -0500

    Release 1.0.0

---
+++ release.ps1
@@ -0,0 +1,20 @@
+$USERNAME=$args[0]
+IMAGE=webwatcher
+
+$version = Get-Content VERSION -Raw 
+echo "version: $version"
+
+# run build
+. build.ps1
+
+# tag it
+git add -A
+git commit -m "Release $version"
+git tag -a "$version" -m "Release $version"
+git push
+git push --tags
+docker tag ${USERNAME}/${IMAGE}:latest ${USERNAME}/${IMAGE}:$version
+
+# push it
+docker push ${USERNAME}/${IMAGE}:latest
+docker push ${USERNAME}/${IMAGE}:$version
